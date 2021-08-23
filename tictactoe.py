ffaeeed4e66eb51bbce501b3ce57a5b8838f0713
NRDubin <nicole.dubin@gmail.com>
2021-08-22 22:20:23 -0700

    Board is now an actual 2d list

--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,7 @@
 class gameBoard:
     def __init__(self, size):
         self.size = size
-        self.board = [[' ']*size]*size
+        self.board = [[' '] * size for i in range(size)]
         
     def printBoard(self):
         rows = list(map(lambda r: " "+" | ".join(r), self.board))#generates list of rows with |
@@ -19,6 +19,7 @@ class playerCells:
     
     #def checkWin(self, winSets):
     #check if any of the win sets are in the player's spaces set
+    
 
 class game:
     def __init__(self) -> None:
